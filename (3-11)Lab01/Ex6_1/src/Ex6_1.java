e5ba826250abc34989b5e1c4a69bf3e258459622
HaoVuNguyen <hao.vn226037@sis.hust.edu.vn>
2024-03-15 22:27:42 +0700

    Lab001

---
+++ (3-11)Lab01/Ex6_1/src/Ex6_1.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+public class Ex6_1 {
+    public static void main(String[] args){
+        int option = JOptionPane.showConfirmDialog(null, "Do you wanna play?", null, 0);
+
+        JOptionPane.showMessageDialog(null, "You have chosen: " + (option==JOptionPane.YES_OPTION?"Yes":"No"));
+        System.exit(0);
+    }
+}
