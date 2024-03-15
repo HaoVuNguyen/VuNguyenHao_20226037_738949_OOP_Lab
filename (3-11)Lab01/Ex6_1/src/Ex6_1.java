5094e85d030e918a8e6161b3577ac64ce3257be1
HaoVuNguyen <hao.vn226037@sis.hust.edu.vn>
2024-03-15 22:59:55 +0700

    Lab001

--- (3-11)Lab01/Ex6_1/src/Ex6_1.java
+++ (3-11)Lab01/Ex6_1/src/Ex6_1.java
@@ -1,7 +1,8 @@
 import javax.swing.JOptionPane;
 public class Ex6_1 {
     public static void main(String[] args){
-        int option = JOptionPane.showConfirmDialog(null, "Do you wanna play?", null, 0);
+        int option = JOptionPane.showConfirmDialog(null, 
+        		"Do you wanna change to the first class ticket?", null, 0);
 
         JOptionPane.showMessageDialog(null, "You have chosen: " + (option==JOptionPane.YES_OPTION?"Yes":"No"));
         System.exit(0);
